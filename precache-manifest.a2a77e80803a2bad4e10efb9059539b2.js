fb890f58fd82bff1ece2dc53c88361ad00fa5ed7
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-04-02 14:34:37 -0400

    Updates

---
+++ precache-manifest.a2a77e80803a2bad4e10efb9059539b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbc7f87b55c3972bb6687017baa7958",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "4ed8286e90c289275e0d",
+    "url": "/youtube-clone/static/js/2.f2f7520e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.f2f7520e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d1d6b2595cdfa5c8dd",
+    "url": "/youtube-clone/static/js/main.336da3f0.chunk.js"
+  },
+  {
+    "revision": "844d4cfab7860bee6e8b",
+    "url": "/youtube-clone/static/js/runtime-main.c0a9cebd.js"
+  }
+]);
